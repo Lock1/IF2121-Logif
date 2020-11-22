f684b2695a2a6399998a041318099b81a7786cc7
fadelananda <fadelananda123@gmail.com>
2020-11-23 00:50:59 +0700

    cleaning and add function

--- infoplayer.pl
+++ infoplayer.pl
@@ -1,32 +1,48 @@
-:- dynamic(inventory/6)
-
-inventoryMax(100).
+:- dynamic(inventory/6).
 
 /*inventory(ItemID, class, category, name, attack, def)*/
-inventoryLength(Length) :-
-    findall(ItemName, inventory(_,_,_,ItemName,_,_), List),
-    length(List,Length).
-
-
-addItem(_) :-
-    cekPanjang(Length),
-    inventoryMax(Max),
-    Length >= Max,
-    write('Maaf bang inventory udah penuh'),
-    !,fail.
-
 addItem(ItemID) :-
+    findall(ItemName, inventory(_,_,_,ItemName,_,_), List),
+    length(List,Length),
+    (
+    Length >= 100,
+    write('Inventory is full'),
+    !,fail;
+
     item(ItemID, ClassType, Category, ItemName, Attack, Def),
-    asserta(inventory(ItemID, Class, Category, ItemName, Attack, Def)),!.
+    asserta(inventory(ItemID, Class, Category, ItemName, Attack, Def)),!
+    ).
 
 delItem(ItemID) :-
     \+inventory(ItemID,_,_,_,_,_),
-    write('Gaada item yang dimaksud bang'),!,fail.
+    write('There is no specified item to delete'),!,fail;
 
-delItem(ItemID) :-
     retract(inventory(ItemID,_,_,_,_,_)), !.
 
-createListInventory(ListItemName, ListCategory) :-
-    findall(ItemName, inventory(_,_,_,ItemName,_,_)),
-    findall(Class, inventory(_,Class,_,_,_,_)),
-    findall()
+listing(List1, List2, List3, List4, List5):-
+    [],[],[],[],[],[];
+    [W1|W2]=List1,
+    write(W1), nl,
+    [X1|X2]=List2,
+    write(X1), nl,
+    [Y1|Y2]=List3,
+    write(Y1), nl,
+    [Z1|Z2]=List4,
+    write(Z1), nl,
+    [A1|A2]=List5,
+    write(A1), nl,
+    listing(W2, X2, Y2, Z2, A2).
+
+listInventory:-
+    findall(ItemName, inventory(_,_,_,ItemName,_,_), Names),
+    findall(Class, inventory(_,Class,_,_,_,_), Classes),
+    findall(ItemID, inventory(ItemID,_,_,_,_,_), IDs),
+    findall(Attack, inventory(_,_,_,_,Attack,_), Attacks),
+    findall(Def, inventory(_,_,_,_,_,Def), Defs),
+    write('----------------'), nl,
+    write('Inventory:'), nl,
+    listing(Names, Classes, IDs, Attacks, Defs).
+
+
+
+
