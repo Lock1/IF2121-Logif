3eec165a1116fdb5ac2feb3a2857ee06784e18f4
fadelananda <fadelananda123@gmail.com>
2020-11-23 14:23:34 +0700

    tambah listing

--- infoplayer.pl
+++ infoplayer.pl
@@ -25,7 +25,7 @@ delItem(ItemID) :-
 
     retract(inventory(ItemID,_,_,_,_,_)), !.
 
-listing(List1, List2, List3, List4, List5):-
+listing(List1, List2, List3, List4):-
     [],[],[],[],[],[];
     [W1|W2]=List1,
     write(W1), nl,
@@ -35,19 +35,41 @@ listing(List1, List2, List3, List4, List5):-
     write(Y1), nl,
     [Z1|Z2]=List4,
     write(Z1), nl,
-    [A1|A2]=List5,
-    write(A1), nl,
-    listing(W2, X2, Y2, Z2, A2).
+    listing(W2, X2, Y2, Z2).
 
-listInventory:-
+listingPotion(List1, List2, List3, List4):-
+    [],[],[],[],[],[];
+    [W1|W2]=List1,
+    write(W1), nl,
+    [X1|X2]=List2,
+    write(X1), nl,
+    [Y1|Y2]=List3,
+    write(Y1), nl,
+    [Z1|Z2]=List4,
+    write(Z1), nl,
+    listing(W2, X2, Y2, Z2).
+
+listItem:-
     findall(ItemName, inventory(_,_,_,ItemName,_,_), Names),
-    findall(Class, inventory(_,Class,_,_,_,_), Classes),
     findall(ItemID, inventory(ItemID,_,_,_,_,_), IDs),
     findall(Attack, inventory(_,_,_,_,Attack,_), Attacks),
     findall(Def, inventory(_,_,_,_,_,Def), Defs),
     write('----------------'), nl,
-    write('Inventory:'), nl,
-    listing(Names, Classes, IDs, Attacks, Defs).
+    write('Weapon:'), nl,
+    listing(Names, IDs, Attacks, Defs).
+
+listPotion:-
+    findall(PotionName, inventory(_,_,_,PotionName,_,_), PNames),
+    findall(ItemID, inventory(ItemID,_,_,_,_,_), IDs),
+    findall(PlusHP, inventory(_,_,_,_,PlusHP,_), HPs),
+    findall(PlusMana, inventory(_,_,_,_,_,_,PlusMana), ManaS),
+    write('----------------'), nl,
+    write('Potion:'), nl,
+    listingP(PNames, IDs, HPs, ManaS).
+
+listInventory:-
+    listItem,
+    listPotion.
 
 
 
